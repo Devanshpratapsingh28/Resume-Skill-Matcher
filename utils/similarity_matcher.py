1abc33d76da1ea80fe8dd9aa182fe883edb3105e
Devansh Pratap Singh <160342233+Devanshpratapsingh28@users.noreply.github.com>
2025-06-28 23:30:51 +0530

    Updated similarity_matcher.py

--- utils/similarity_matcher.py
+++ utils/similarity_matcher.py
@@ -13,7 +13,7 @@ def calc_similarity(resume_path,jd_text):
     
   vectors = model.encode([resume_preprocess_text, jd_preprocess_text])
     
-  similarity = cosine_similarity([vectors[0]], [vectors[1]])
+  similarity = max(cosine_similarity([vectors[0]], [vectors[1]]),0)
   percentage_similarity = round(similarity[0][0] * 100, 2)
     
   return percentage_similarity
